7d7d5ed6ca704ae930503e7ad4a4e36e234f6e0c
hyperclick <forspam28@ya.ru>
2020-01-27 23:15:37 +0300

    move tests to 42sh_tests project

--- src/bin/42sh/main.c
+++ src/bin/42sh/main.c
@@ -1,14 +1,7 @@
 #include "cc.h"
-#include "history_tests.h"
-#include "history.h"
-#include <stdio.h>
-
-
-
 
 int main(int ac, char *av[], char *en[])
 {
 	fdputs("Hello, 42sh!\n", 1);
-	test_history();
 	return (0);
 }
