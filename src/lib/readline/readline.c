9d28ffc0707613ca637cb2cb2cc414604351ea2b
kosyan62 <Kosyan62@gmail.com>
2020-08-16 19:18:57 +0300

    Started a readline

--- src/lib/readline/readline.c
+++ src/lib/readline/readline.c
@@ -3,8 +3,51 @@
 //
 
 #include "readline.h"
+#include <string.h>
+#include <stdio.h>
 
-int readline(char **line)
+
+int		get_key_func(char key[4])
 {
-
+	if (!strcmp(key, "\33"))
+		return (0); //TODO escape character
+	else if (!strcmp(key, "\33\133\101") || !(strcmp(key, "\20")))
+		return (0); //TODO arrow up
+	else if (!strcmp(key, "\33\133\102") || !(strcmp(key, "\16")))
+		return (0); //TODO arrow down
+	else if (!strcmp(key, "\33\133\103") || !(strcmp(key, "\6")))
+		return (0); //TODO arrow right)
+	else if (!strcmp(key, "\33\133\104") || !(strcmp(key, "\2")))
+		return (0); //TODO arrow left
+	else if (!strcmp(key, "\177"))
+		return (0); //TODO backspase. del previous symbol;
+	else if (key[0] == 27 && key[1] == 91 && key[2] == 51 && key[3] == 126)
+		return (0); //TODO del. delete
+	else if (!strcmp(key, "\t"))
+		return (0); //TODO tabulation. autocomplete
+	else if (!strcmp(key, "\n"))
+		return (1); //enter return notnull
+	else
+	{
+		printf("%o ", key[0]);
+		printf("%o ", key[1]);
+		printf("%o ", key[2]);
+		printf("%o\n", key[3]);
+		return 0;
+	}
 }
+int		readline(char **line)
+{
+	char key[4];
+
+	while (42)
+	{
+		bzero(key, 4);
+		read(STDIN_FILENO, key, 4);
+		if (get_key_func(key))
+		{
+			return 0;
+		}
+	}
+}
+
--- src/lib/readline/readline.h
+++ src/lib/readline/readline.h
@@ -1,10 +1,43 @@
-//
-// Created by Darth Butterwell on 8/16/20.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readline.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgena <mgena@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/16 16:20:02 by mgena             #+#    #+#             */
+/*   Updated: 2020/08/16 16:45:01 by mgena            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef READLINE_H
 #define READLINE_H
 
-int readline(char **line);
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <termios.h>
+# include <sys/ioctl.h>
+# include <stdbool.h>
+# include <term.h>
+# include <sys/stat.h>
+
+typedef struct	s_out
+{
+	struct termios	savetty;
+	char			tbuf[2048];
+	char			strings[256];
+	char			*p;
+	char			*tname;
+	int				fd;
+}				t_out;
+
+t_out g_out;
+
+int			readline(char **line);
+void		main_init(void);
+void		return_tty(void);
+void 		ft_putstr(char *str);
+int			ft_put(int c);
 
 #endif //READLINE_H
