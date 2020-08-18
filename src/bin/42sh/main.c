b9186b7eac9bdd9002d5f65ee8664e8866ead95f
dbutterw <dbutterw@student.21-school.ru>
2020-08-18 22:45:19 +0300

    vec fix gnl fix

--- src/bin/42sh/main.c
+++ src/bin/42sh/main.c
@@ -1,14 +1,22 @@
 #include "cc.h"
-#include "expand.h"
 
 int main(int ac, char *av[], char *en[])
 {
 //	fdputs("Hello world!", 1);
 //	return (0);
 
-	char *str = strdup("aaa  \"bb \\\n $((\\\nHOME\\\n)) vvv\" 'aaa \\\n bbbbb' ");
-	int error = handle_raw(&str, e_validate);
-	fdputs(str, 1);
-	free(str);
-	exit(error);
+	t_vec *vec;
+	vec = vec_new(15, 1, NULL);
+	int cnt = 0;
+	while (av[0][cnt])
+		vec_push(vec, &av[0][cnt++]);
+	char n = '9';
+	vec_push_at(vec, &n, 20);
+	cnt++;
+	char *str = vec->data;
+	putendl(str);
+	while (cnt--)
+		vec_rm_last(vec);
+	puts(vec->data);
+	vec_del(&vec);
 }
