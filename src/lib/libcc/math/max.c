2895d581827b1028c6538990ff7a95ad488f2a52
dbutterw <dbutterw@student.21-school.ru>
2020-08-22 22:16:16 +0300

    submodule fix

--- src/lib/libcc/math/max.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-ssize_t max(ssize_t x, ssize_t y)
-{
-	if (x < y)
-		return (y);
-	return (x);
-}
--- src/lib/libcc/math/min.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-ssize_t min(ssize_t x, ssize_t y)
-{
-	if (x > y)
-		return (y);
-	return (x);
-}
