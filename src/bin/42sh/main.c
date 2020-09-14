d9118c66cab5288d11f7caf58d26054a8972ff7e
Slava-Nya <svyatoslavank@gmail.com>
2020-09-14 20:18:24 +0300

    Merge branch 'develop' into builtins
    # Conflicts:
    #	src/CMakeLists.txt
    #	src/bin/42sh/main.c
    #	src/lib/readline/readline.h

--- src/bin/42sh/main.c
+++ src/bin/42sh/main.c
@@ -3,12 +3,19 @@
 //
 
 #include "main.h"
+#include "builtins.h"
+
+//int main(int ac, const char *av[], const char *env[])
+//{
+//	int error;
+//
+//	if (!(error = main_init(av[0], av + 1, env)))
+//		error = main_manager();
+//	return (error);
+//}
 
 int main(int ac, const char *av[], const char *env[])
 {
-	int error;
-
-	if (!(error = main_init(av[0], av + 1, env)))
-		error = main_manager();
-	return (error);
+	cd(ac, av);
+	return (0);
 }
