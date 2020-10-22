04aaec39f09eb65e037871cca806c056244142d4
Fidelia Mallister <fmallist@at-p5.msk.21-school.ru>
2020-10-22 18:43:29 +0300

    WIP

--- src/lib/libexec/exec_simple_cmd.c
+++ src/lib/libexec/exec_simple_cmd.c
@@ -5,40 +5,12 @@
 #include "exec.h"
 #include "env.h"
 
-static t_token_type get_redirect_type(t_token *redir_token)
-{
-	return (redir_token->type);
-}
-
-static void	fill_process_with_redirects(t_ast **redirects, t_process *process)
-{
-	size_t i;
-	t_token_type redir;
-
-	redir = 0;
-	i = 0;
-	while (redirects[i])
-	{
-		if (redirects[i]->type == p_io_redirect)
-		{
-			redir = get_redirect_type(redirects[i]->token);
-			// TODO: switch redir and handle it's redirection
-		}
-		i++;
-	}
-}
-
 void		exec_simple_cmd(t_ast *ast, t_process *process)
 {
-	t_ast **redirects;
-
-	redirects = NULL;
 	if (expand_ast(ast) || prepare_redirect(ast, process))
 		return ;
 	process->argv = get_args(ast);
 	prepare_exec_env(ast->left);
 	process->env = exec_env_2array();
-	redirects = get_redirect_nodes(ast);
-	fill_process_with_redirects(redirects, process);
 	exec_env_del();
 }
--- src/lib/libexec/prepare_redirect.c
+++ src/lib/libexec/prepare_redirect.c
@@ -3,8 +3,14 @@
 //
 
 #include "exec.h"
-
+#include <stdio.h> // delete this
 int 	prepare_redirect(t_ast *ast, t_process *process)
 {
+	size_t i;
+	t_ast **redirects;
+
+	if ((redirects = get_redirect_nodes(ast)) == NULL)
+		return (0);
+	//... WIP
 	return (0);
 }
