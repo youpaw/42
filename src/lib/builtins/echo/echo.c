345a66c2fc0a8dd8473bf54a5ab442c0a918e1d3
Slava-Nya <svyatoslavank@gmail.com>
2020-11-07 23:55:36 +0300

    echo refactor spec chars ended

--- src/lib/builtins/echo/echo.c
+++ src/lib/builtins/echo/echo.c
@@ -4,6 +4,27 @@
 
 #include "echo.h"
 
+void		printspchars(char *str)
+{
+	size_t cnt;
+
+	cnt = 0;
+	while (str[cnt])
+	{
+		if (str[cnt] == '\\')
+		{
+			str += cnt;
+			cnt = 0;
+			if (!(str = handle_esc_chars(str)))
+				return ;
+		}
+		if (str[cnt] && str[cnt] != '\\')
+		{
+			write(1, &str[cnt], 1);
+			cnt++;
+		}
+	}
+}
 
 static int			check_opt(const char  **av, unsigned char *flags)
 {
@@ -13,8 +34,10 @@ static int			check_opt(const char  **av, unsigned char *flags)
 	skip_args = optparse(av, "en", &opt);
 	if (skip_args > 1)
 	{
-		if (opt.options[0] == 'n')
-			*flags = ECHO_N_FLAG;
+		if (strchr(opt.options, 'n') != NULL)
+			*flags |= ECHO_n_FLAG;
+		if (strchr(opt.options, 'e') != NULL)
+			*flags |= ECHO_e_FLAG;
 		free(opt.options);
 	}
 	if (skip_args == 0)
@@ -31,12 +54,15 @@ int					echo(const char **av)
 	arg_cnt = check_opt(av, &flags);
 	while (av[arg_cnt])
 	{
-		puts(av[arg_cnt]);
+		if (flags & ECHO_e_FLAG)
+			printspchars((char *) av[arg_cnt]);
+		else
+			puts(av[arg_cnt]);
 		if (av[arg_cnt + 1])
 			putchar(' ');
 		arg_cnt++;
 	}
-	if (flags & ECHO_E_FLAG)
+	if (!(flags & ECHO_n_FLAG))
 		putchar('\n');
 	return (0);
 }
