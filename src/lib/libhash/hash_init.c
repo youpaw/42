ff3047375902e7a42d4281df5fa79553952f4052
Fidelia Mallister <fmallist@at-p7.msk.21-school.ru>
2020-11-10 15:13:34 +0300

    libhash normed

--- src/lib/libhash/hash_init.c
+++ src/lib/libhash/hash_init.c
@@ -8,19 +8,19 @@
 
 t_hash_map *g_hash;
 
-static int 		hash_cmp_bin(const t_hash_pair *lhs, const t_hash_pair *rhs)
+static int		hash_cmp_bin(const t_hash_pair *lhs, const t_hash_pair *rhs)
 {
 	return (strcmp(lhs->key, rhs->key));
 }
 
-static void 	hash_del_bin(t_hash_pair *pair)
+static void		hash_del_bin(t_hash_pair *pair)
 {
 	free(pair->key);
 	free(((t_hash_bin_pair *)pair->value)->path);
 	free(pair->value);
 }
 
-void	hash_init()
+void			hash_init(void)
 {
 	g_hash = hash_map_new(N_MAX_HASH, (size_t (*)(const void *)) &strhash, \
 		(int (*)(const void *, const void *)) &hash_cmp_bin, &hash_del_bin);
