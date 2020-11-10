267c04e4313afe817be4a17fc03b7fce49e028fa
Fidelia Mallister <fmallist@at-p7.msk.21-school.ru>
2020-11-10 21:23:31 +0300

    segmentation fault fixed

--- src/lib/libjob/run_builtin.c
+++ src/lib/libjob/run_builtin.c
@@ -10,6 +10,6 @@ int run_builtin(t_process *p)
 	int index;
 
 	if ((index = get_builtin_index(p->argv[0])) != -1)
-		run_builtin_by_index(p, index);
+		return (run_builtin_by_index(p, index));
 	return (-1);
 }
