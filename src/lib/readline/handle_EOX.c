4cc666c780ce82affe327be28c8e20ba3acf16b7
dbutterw <dbutterw@student.21-school.ru>
2020-11-12 04:17:53 +0300

    g_exit code fix

--- src/lib/readline/handle_EOX.c
+++ src/lib/readline/handle_EOX.c
@@ -4,11 +4,13 @@
 
 #include "readline.h"
 #include <unistd.h>
+#include "env.h"
 
 int			handle_eox(t_inp *inp)
 {
 	put_cursor_to_the_end(inp);
 	write(STDOUT_FILENO, "\n", 1);
 	del_input(inp);
+	g_exit_code = 1;
 	return(1);
 }
