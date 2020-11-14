c3d8df07e01cc916ac69bf4644e9f0150a29f728
dbutterw <dbutterw@student.21-school.ru>
2020-11-14 14:33:11 +0300

    redirect fix

--- src/lib/libjob/redirect_heredoc.c
+++ src/lib/libjob/redirect_heredoc.c
@@ -1,6 +1,14 @@
-//
-// Created by Fidelia Mallister on 11/4/20.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect_heredoc.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbutterw <dbutterw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 14:52:34 by dbutterw          #+#    #+#             */
+/*   Updated: 2020/11/12 19:11:12 by dbutterw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "jobs.h"
 #include <unistd.h>
@@ -12,7 +20,8 @@ int		redirect_heredoc(t_ast *leafs, t_process *process)
 
 	if (pipe(pfd) < 0)
 		return (1);
-	close(process->stdin);
+	if (!is_standard_io(process->stdin))
+		close(process->stdin);
 	fdputs(leafs->left->left->token->raw, pfd[1]);
 	process->stdin = pfd[0];
 	close(pfd[1]);
