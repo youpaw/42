99d8bb4e787a9b6e2ada98389f3d3e6b6c5a06bc
Azzak Omega <azomega@at-q5.msk.21-school.ru>
2020-11-20 19:35:15 +0300

    redirection great fixed

--- src/lib/libjob/redirect_great.c
+++ src/lib/libjob/redirect_great.c
@@ -19,7 +19,7 @@ int		redirect_great(t_ast *leafs, t_process *process, int is_double_great)
 	int from;
 	int to;
 
-	options = O_RDWR | O_CREAT | (is_double_great ? O_APPEND : O_TRUNC);
+	options = O_WRONLY | O_CREAT | (is_double_great ? O_APPEND : O_TRUNC);
 	from = redirect_parse_left_side(leafs, 1);
 	to = redirect_parse_right_side(leafs->left->left->token, options, 0, 0);
 	if (to == 1 || to == -1)
